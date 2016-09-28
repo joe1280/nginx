db336675b3647ede482c807dad52ba95f0160f95
joe1280 <4334838@qq.com>
2016-09-28 23:27:10 +0800

    Update keepalived.sh

--- keepalived.sh
+++ keepalived.sh
@@ -2,7 +2,7 @@
 #这个是keeplive的安装脚本
 yum install -y gcc openssl-devel popt-devel lrzsz
 wget   --no-check-certificate  https://github.com/joe1280/nginx/raw/master/keepalived-1.2.24.tar.gz
-tar keepalived-1.2.24.tar.gz
+tar zxvf keepalived-1.2.24.tar.gz
 cd keepalived-1.2.24
 ./configure
 make && make install 
