e0b89459b54de5d46f984bed69b7c1ede30ec3e1
joe1280 <4334838@qq.com>
2016-10-01 18:25:39 +0800

    memcached-memcached_php.sh
    memcached-memcached_php.sh

---
+++ memcached-memcached_php.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/memcached.sh && bash memcached.sh
+wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/memcached_php.sh && bash memcached_php.sh
