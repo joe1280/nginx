aa01748987f568f8786c609ee7b9422c79fae274
joe1280 <4334838@qq.com>
2016-10-01 21:52:19 +0800

    redis 服务端安装 和 php 扩展安装脚本
    redis 服务端安装 和 php 扩展安装脚本

---
+++ redis-phpredis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/redis.sh && bash redis.sh
+wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/redis-php.sh && redis-php.sh
