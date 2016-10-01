110bc07189c192ea9f0badedaf078f9fcbcc39c6
joe1280 <4334838@qq.com>
2016-10-01 21:58:25 +0800

    Update redis-phpredis.sh

--- redis-phpredis.sh
+++ redis-phpredis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/redis.sh && bash redis.sh
-wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/redis-php.sh && redis-php.sh
+wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/redis-php.sh && bash redis-php.sh
