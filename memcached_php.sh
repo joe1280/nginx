c67c5f020d33164aa7587a32108abbee5aa1e164
joe1280 <4334838@qq.com>
2016-10-01 18:10:25 +0800

    结合lnmp的 memecached php 扩展
    结合lnmp的 memecached php 扩展

---
+++ memcached_php.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget  --no-check-certificate https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+tar xzvf libmemcached-1.0.18.tar.gz
+cd libmemcached-1.0.18
+./configure -prefix=/usr/local/libmemcached -with-memcached
+make && make install
+yum install php-devel
+wget --no-check-certificate http://pecl.php.net/get/memcached-2.2.0.tgz
+tar xzvf memcached-2.2.0.tgz
+cd memcached-2.2.0
+/usr/local/php/bin/phpize
+./configure --disable-memcached-sasl -enable-memcached -with-php-config=/usr/local/php/bin/php-config -with-zlib-dir -with-libmemcached-dir=/usr/local/libmemcached -prefix=/usr/local/phpmemcached
+make && make install 
+cp /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/memcached.so /usr/local/php/lib/php/extension/
+rm -rf /usr/local/php/etc/php.ini
+cd /usr/local/php/etc/
+wget --no-check-certificate https://raw.githubusercontent.com/joe1280/nginx/master/php.ini
+service nginx restart
+service php-fpm reload
