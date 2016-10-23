9a9fd6723846dd9bfdd4323fec2ef06fc7d756e6
joe1280 <4334838@qq.com>
2016-10-23 20:38:09 +0800

    更改日期
    更改日期

--- redis-php.sh
+++ redis-php.sh
@@ -5,7 +5,7 @@ cd phpredis
 /usr/local/php/bin/phpize
 ./configure --with-php-config=/usr/local/php/bin/php-config
 make && make install
-cp /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/redis.so /usr/local/php/lib/php/extensions/
+cp /usr/local/php/lib/php/extensions/no-debug-non-zts-20131226/redis.so /usr/local/php/lib/php/extensions/
 echo 'extension=redis.so' >> /usr/local/php/etc/php.ini 
 echo 'extension_dir=/usr/local/php/lib/php/extensions' >> /usr/local/php/etc/php.ini 
 service nginx restart
