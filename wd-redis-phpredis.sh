70858f688b4a1ffbe2002bd3c2bc119924a8face
joe1280 <4334838@qq.com>
2016-11-09 03:41:55 +0800

    Delete wd-redis-phpredis.sh

--- wd-redis-phpredis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://github.com/joe1280/nginx/raw/master/wdlinux-redis  && bash wdlinux-redis
-wget https://github.com/joe1280/nginx/blob/master/wd-redis-php.sh  && bash wd-redis-php.sh
