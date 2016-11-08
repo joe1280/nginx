88e777b639ca84c34d1137b554a57325dcf55d93
joe1280 <4334838@qq.com>
2016-11-09 04:26:02 +0800

    Update wd-redis-phpredis.sh

--- wd-redis-phpredis.sh
+++ wd-redis-phpredis.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #安装服务端
 wget https://github.com/joe1280/nginx/raw/master/wdlinux-redis  && bash wdlinux-redis
-wget https://github.com/joe1280/nginx/blob/master/wd-redis-php.sh  && bash wd-redis-php.sh
+wget https://raw.githubusercontent.com/joe1280/nginx/master/wd-redis-php.sh  && bash wd-redis-php.sh
