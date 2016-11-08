5578187a453d8f7c724902fb120814832383eaca
joe1280 <4334838@qq.com>
2016-11-09 04:24:13 +0800

    Update wd-redis-php.sh

--- wd-redis-php.sh
+++ wd-redis-php.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum -y install git lrzsz unzip
 wget https://github.com/joe1280/nginx/raw/master/phpredis-develop.zip
-unzip phpredis-develop.zip
+unzip phpredis-develop
 cd phpredis-develop
