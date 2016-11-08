847e2df57569773736254b16db4f8d07a7c1f9a7
joe1280 <4334838@qq.com>
2016-11-09 03:34:17 +0800

    Update wd-redis-php.sh

--- wd-redis-php.sh
+++ wd-redis-php.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 yum -y install git lrzsz unzip
 wget https://github.com/joe1280/nginx/raw/master/phpredis-develop.zip
 unzip phpredis-develop.zip
@@ -6,8 +6,6 @@ cd phpredis-develop
 /www/wdlinux/php/bin/phpize
 ./configure --with-php-config=/www/wdlinux/php/bin/php-config
 make && make install
-#cp /www/wdlinux/php/lib/php/extensions/no-debug-non-zts-20131226/redis.so /www/wdlinux/php/lib/php/extensions/
 echo 'extension=redis.so' >> /www/wdlinux/php/etc/php.ini 
-#echo 'extension_dir=/www/wdlinux/nginx_php-5.6.21/lib/php/extensions/no-debug-non-zts-20131226' >> /www/wdlinux/php/etc/php.ini 
 service nginxd restart
 service php-fpm reload
